5eeda7be8648e18bed4aa63880e99a97e063d89c
Julio Cesar <jccintr@gmail.com>
2026-07-28 21:36:17 -0300

    Criado Admin e Delivery models

---
+++ models/delivery.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const deliverySchema = new mongoose.Schema({
+
+ data:{
+        type:Date,
+        default:Date.now
+ },
+ store:{
+        type: mongoose.Types.ObjectId,
+        ref: "Store",
+        required:true
+ },
+ rider:{
+        type: mongoose.Types.ObjectId,
+        ref: "Rider",
+        default:null
+ },
+ origem:{latitude:Number,longitude:Number,address:String},
+ destino:{latitude:Number,longitude:Number,address:String},
+ distancia: { 
+    type:Number,
+    required:true
+ },
+ status:{
+        type:Number,
+        default:0
+ },
+ riderRating:{
+        type:Number,
+        required:false,
+        default: null
+ },
+ events:[
+  {
+    data:{ type:Date,},
+    descricao:{type:String,}       
+  }
+ ],
+
+}, { timestamps: true });
+
+const Delivery = mongoose.model('Delivery', storeSchema);
+export default Delivery;
